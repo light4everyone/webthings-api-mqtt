f0d9ae65c5d745ff86a9fd4c1dee7b845da93feb
light4everyone <jenea94@gmail.com>
2019-12-25 23:15:21 +0200

    remove useless packages

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dnssd from 'dnssd';
-
-// function startMdns() {
-//   const mdns = new dnssd.Advertisement(
-//     new dnssd.ServiceType('_webthing._tcp'),
-//     8888,
-//     {
-//       name: 'mqtt-cloud',
-//       txt: {
-//         path: '/',
-//       },
-//     });
-//   mdns.on('error', e => {
-//     // tslint:disable-next-line: no-console
-//     console.debug(e);
-//     setTimeout(() => {
-//       this.mdns.start();
-//     }, 10000);
-//   });
-
-//   mdns.start();
-// }
 
 async function bootstrap() {
-  // startMdns();
-
   const app = await NestFactory.create(AppModule);
   await app.listen(8888);
 }
+
 bootstrap();
