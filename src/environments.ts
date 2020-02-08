06212ed96bf59e3bece3a1a94937e237d69a1a03
light4everyone <jenea94@gmail.com>
2020-02-08 14:44:46 +0200

    rename

--- src/environments.ts
+++ src/environments.ts
@@ -1,6 +1,6 @@
 export const env = {
   mongodb: {
-    mongoDbConnection: process.env.MONGO_DB_CONNECTION ? process.env.MONGO_DB_CONNECTION : 'mongodb://localhost:27017'
+    mongoDbConnection: process.env.MONGO_DB_URL ? process.env.MONGO_DB_URL : 'mongodb://localhost:27017'
   },
   mosquitto: {
     mosquittoUrl: process.env.MOSQUITTO_URL ? process.env.MOSQUITTO_URL : 'mqtt://localhost'
